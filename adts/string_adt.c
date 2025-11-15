b259c51a04e612b87aa193f0cd18ee505d29b6d1
DanielFablicio <fabliciodaniel@gmail.com>
2025-11-15 11:10:51 -0300

    fix: inverted assertion logic in string_erase()

--- adts/string_adt.c
+++ adts/string_adt.c
@@ -151,8 +151,8 @@ string_t string_insert(string_t s, size_t pos, const char *substr) {
 
 string_t string_erase(string_t s, size_t pos, size_t len) {
     assert(s != NULL);
-    assert(pos > s->len);
-    assert(len > (s->len - pos));
+    assert(pos < s->len);
+    assert(len <= (s->len - pos));
 
     strcpy(&s->buf[pos], &s->buf[pos + len]);
     return s;
