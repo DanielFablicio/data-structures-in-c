96c02e7919dcfc6a50fe4a1ba76b1e466eef3bc3
DanielFablicio <fabliciodaniel@gmail.com>
2025-11-17 10:44:18 -0300

    test: starts testing

---
+++ tests/test_string_adt.c
@@ -0,0 +1,116 @@
+#include "framework/unity.h"
+#include "../adts/string_adt.h"
+#include <string.h>
+
+string_t s1 = NULL;
+string_t s2 = NULL;
+
+void setUp(void) {}
+
+void tearDown(void) {
+    string_delete(s1);
+    s1 = NULL;
+
+    if (s2 != NULL) {
+        string_delete(s2);
+        s2 = NULL;
+    }
+}
+
+void test_string_new(void) {
+    s1 = string_new();
+
+    TEST_ASSERT(string_len(s1) == 0);
+    TEST_ASSERT(string_cap(s1) != 0);
+    TEST_ASSERT(string_cstr(s1)[0] == '\0');
+}
+
+void test_string_init(void) {
+    const char *cstr = "Example sentence.";
+    const size_t len = strlen(cstr);
+
+    s1 = string_init(cstr);
+
+    TEST_ASSERT_EQUAL_STRING(cstr, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(len, string_len(s1));
+}
+
+void test_string_insert(void) {
+    s1 = string_init("to be question");
+
+    const char *cstr = "the ";
+    string_insert(s1, 6, cstr);
+
+    const char *expected = "to be the question";
+    size_t expected_len = strlen(expected);
+
+    TEST_ASSERT_EQUAL_STRING(expected, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(expected_len, string_len(s1));
+}
+
+void test_string_erase_1(void) {
+    s1 = string_init("This is an example sentence.");
+    string_erase(s1, 10, 8);
+
+    const char *expected = "This is an sentence.";
+    const size_t expected_len = strlen(expected);
+
+    TEST_ASSERT_EQUAL_STRING(expected, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(expected_len, string_len(s1));
+}
+
+void test_string_erase_2(void) {
+    s1 = string_init("This is an example sentence.");
+    string_erase(s1, 18, 9);
+
+    const char *expected = "This is an example.";
+    const size_t expected_len = strlen(expected);
+
+    TEST_ASSERT_EQUAL_STRING(expected, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(expected_len, string_len(s1));
+}
+
+void test_string_erase_3(void) {
+    s1 = string_init("This is an example sentence.");
+    string_erase(s1, 20, 0);
+
+    const char *expected = "This is an example sentence.";
+    const size_t expected_len = strlen(expected);
+
+    TEST_ASSERT_EQUAL_STRING(expected, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(expected_len, string_len(s1));
+}
+
+void test_string_swap(void) {
+    const char *cstr1 = "an example sentence.";
+    const char *cstr2 = "an example phrase.";
+    const size_t len1 = strlen(cstr1);
+    const size_t len2 = strlen(cstr2);
+
+    s1 = string_init(cstr1);
+    s2 = string_init(cstr2);
+
+    const size_t cap1 = string_cap(s1);
+    const size_t cap2 = string_cap(s2);
+
+    string_swap(s1, s2);
+
+    TEST_ASSERT_EQUAL_STRING(cstr1, string_cstr(s2));
+    TEST_ASSERT_EQUAL_STRING(cstr2, string_cstr(s1));
+    TEST_ASSERT_EQUAL_size_t(len1, string_len(s2));
+    TEST_ASSERT_EQUAL_size_t(len2, string_len(s1));
+    TEST_ASSERT_EQUAL_size_t(cap1, string_cap(s2));
+    TEST_ASSERT_EQUAL_size_t(cap2, string_cap(s1));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_string_new);
+    RUN_TEST(test_string_init);
+    RUN_TEST(test_string_insert);
+    RUN_TEST(test_string_erase_1);
+    RUN_TEST(test_string_erase_2);
+    RUN_TEST(test_string_erase_3);
+    RUN_TEST(test_string_swap);
+    return UNITY_END();
+}
